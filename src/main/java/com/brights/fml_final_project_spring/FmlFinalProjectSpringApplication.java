dedf88ad488d8bbf7f83afad352872eb276c19bb
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-12-12 09:28:21 +0100

    Initial commit

---
+++ src/main/java/com/brights/fml_final_project_spring/FmlFinalProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.brights.fml_final_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmlFinalProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmlFinalProjectSpringApplication.class, args);
+    }
+
+}
