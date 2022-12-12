9d23b9630bd93667f7d7a300ca0227bd8de07f3b
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-12-12 10:13:47 +0100

    id-s to models

--- src/main/java/com/brights/fml_final_project_spring/model/Message.java
+++ src/main/java/com/brights/fml_final_project_spring/model/Message.java
@@ -1,4 +1,17 @@
 package com.brights.fml_final_project_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table
+@JsonIgnoreProperties("hibernateLazyInitializer")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
 }
