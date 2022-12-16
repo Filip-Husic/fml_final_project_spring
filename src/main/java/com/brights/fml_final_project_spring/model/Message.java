15bd484f999eff0a725c7e24b0163a825a8a71c9
Lana Pavić <lana.pavic@t.ht.hr>
2022-12-16 10:23:01 +0100

    Message controller - check if it's okay (connections)

--- src/main/java/com/brights/fml_final_project_spring/model/Message.java
+++ src/main/java/com/brights/fml_final_project_spring/model/Message.java
@@ -1,12 +1,15 @@
 package com.brights.fml_final_project_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table
+@Table(name = "messages")
 @JsonIgnoreProperties("hibernateLazyInitializer")
 public class Message {
     @Id
@@ -14,4 +17,41 @@ public class Message {
     @Getter
     @Setter
     private Long id;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private String content;
+
+    @Getter
+    @Setter
+    @Column(name = "posted_date", nullable = false)
+    private LocalDateTime postedDate;
+
+
+    @ManyToOne
+    @Getter
+    @Setter
+    @JsonIgnore
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
+
+    public Message() {
+
+    }
+
+    public Message(String content, LocalDateTime postedDate, User user, User recipient) {
+        this.content = content;
+        this.postedDate = postedDate;
+        this.user = user;
+        this.recipient = recipient;
+    }
 }
+
+
